f5c4ae0e8fb8c03d56201e58776256ac077bb538
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-07 06:27:51 -0700

    Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,8 @@
 
 if __name__ == "__main__":
     """print the result of 1 and 2."""
-
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
