ca24faeb0c09b37a6a76e3cbc0f8ab5f0e0d3217
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-07 07:01:01 -0700

    Python - import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """does some Maths, and prints the result:"""
+    """prints the add,sub,mul, div of 10 and 5"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
+    
