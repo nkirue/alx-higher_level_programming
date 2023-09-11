5ad6e4d5467fa8b66d14ccd07f2ea7f92a46375f
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-11 14:06:46 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
-    for n in my_list:
-        print("{}".format(n))
+    for n in range(len(my_list)):
+        print("{:d}".format(my_list[n]))
