1f3168a22243d8bc25c85628c1c35d4f3be4fc8b
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-13 17:08:16 -0700

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    nom = 0
+    don = 0
+
+    for x in my_list:
+        nom += t[0] * t[1]
+        don += t[1]
+
+    return (nom / don)
