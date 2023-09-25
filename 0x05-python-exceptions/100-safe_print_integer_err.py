6f327985857d64e627b9844ab7af91d7433f5cd6
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-25 10:05:51 -0700

    Python - Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,9 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
-    """
-    A function that prints an integer
-    """
+    """ A function that prints an integer"""
     try:
         print("{:d}".format(value))
         return (True)
