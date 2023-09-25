d4659be04730c9e896d8ee3ee608f5bc46c91323
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-25 06:56:58 -0700

    Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+# iterate throu.  my_list and print elements until reaching x or the end of the my_list
+    cnt = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            cnt += 1
+        except IndexError:
+            break
+    print("")
+    return (cnt)
