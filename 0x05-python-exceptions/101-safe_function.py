0243fb70a498910d1cca20b70122c224a5504c6b
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-25 10:13:01 -0700

    Python - Exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    a function that executes a function safely
+    """
+    try:
+        res = fct(*args)
+        return (res)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
+
+
