7a28a17980c75848f9eb037db3378c487c2f9898
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-26 18:26:04 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines a class Square."""
+
+
+class Square:
+    """This represents a square."""
+
+    def __init__(self, size=0):
+        """method takes an optional size parameter with a default value of 0"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
