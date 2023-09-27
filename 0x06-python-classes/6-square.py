daaca716208c70e17fc0ed1036f8a4d3dcd171fb
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-09-26 19:55:54 -0700

    Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,22 +5,17 @@
 class Square:
     """This represents a square."""
 
-    def __init__(self, size=0):
-        """method takes an optional size parameter with a default value of 0"""
+    def __init__(self, size=0, position=(0, 0)):
+        """method takes size parameter with a default value of 0
+           and position with (0, 0) of the new square.
+        """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Get/set methods defined with the @property and @size.setter decor"""
+        """Get/set methods defined with the @property and @size.setter deco"""
         return (self.__size)
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or 
-	len(value) != 2 or 
-	not all(isinstance(x, int) and x >= 0 for x in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     @size.setter
     def size(self, value):
@@ -30,16 +25,32 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set method of the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
-        """The method returns the computed area"""
+        """The method returns the computed area."""
         return self.__size ** 2
 
     def my_print(self):
-        """method is defined as a public instance method of the Square class"""
+        """method is defined as a public"""
         if self.__size == 0:
             print("")
-        else:
-	for i in range(self.__position[1]):
-	 	print()
-	for i in range(self.__size):
-		print(" " * self.__position[0] + "#" * self.__size)
+            return
+
+        [print("") for x in range(0, self.__position[1])]
+        for x in range(0, self.__size):
+            [print(" ", end="") for y in range(0, self.__position[0])]
+            [print("#", end="") for z in range(0, self.__size)]
+            print("")
