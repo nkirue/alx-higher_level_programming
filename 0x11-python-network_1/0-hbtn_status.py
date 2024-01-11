bb0deb7b799d768fcd40415ec5dfbfbc5d21b891
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-11 13:44:57 -0800

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
 """
-Fetches https://alx-intranet.hbtn.io/status using the urllib package.
+Module description goes here.
 """
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+def fetch_status(url):
+    """
+    Fetches the status from the given URL using the urllib package.
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+    Args:
+    - url (str): The URL to fetch the status from.
+
+    Returns:
+    - str: The body of the response.
+    """
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        return body.decode('utf-8')
+
+if __name__ == "__main__":
+    # Example usage
+    url = 'https://alx-intranet.hbtn.io/status'
+    response_body = fetch_status(url)
     print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body.decode('utf-8'))
+    print("\t- type:", type(response_body))
+    print("\t- content:", response_body)
