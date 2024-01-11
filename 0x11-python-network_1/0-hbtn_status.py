c02dcdd33f595c3ae18c0d5c65eeddc980487d5d
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-11 13:40:30 -0800

    Python - Network #1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using the urllib package.
+"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body.decode('utf-8'))
