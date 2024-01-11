d75770908457c159debe403dcdacb26cc8348855
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-11 07:05:37 -0800

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # URL as an arg., sends a req., & displays d  size of d resp. body in bytes.
-
-curl -sI $1 | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request, and displays the body of a 200 status code response.
-
-curl -sL -w "%{http_code}" $1 -o /dev/null
-
+# GET request, and displays the body of a 200 status code response
+curl -sL "$1"
