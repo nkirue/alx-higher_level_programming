731a0f8050f098db91354d8f7c09248849c77c03
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-10 17:36:02 -0800

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that 
-# URL using curl, and displays the size of the response body in bytes.
+# URL as an arg., sends a req., & displays d  size of d resp. body in bytes.
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Use curl with silent mode to send a request and display 
-# the size of the response body in bytes
-size=$(curl -sI $url | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$size" ]; then
-    echo "Failed to retrieve content size."
-    exit 1
-fi
-
-echo "$size"
-
+curl -sI $1 | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
