05e2e168d720c3f5e64ee75bb7d667dfbb1ff1cc
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-11 08:54:51 -0800

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,18 +7,18 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-   lo_ = 0
+    lo = 0
     hi = len(list_of_integers)
-    cen = ((hi -lo_) // 2) +lo_
-    cen = int(cen)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
     if hi == 1:
         return list_of_integers[0]
     if hi == 2:
         return max(list_of_integers)
-    if list_of_integers[cen] >= list_of_integers[cen - 1] and\
-            list_of_integers[cen] >= list_of_integers[cen + 1]:
-        return list_of_integers[cen]
-    if cen > 0 and list_of_integers[cen] < list_of_integers[cen + 1]:
-        return find_peak(list_of_integers[cen:])
-    if cen > 0 and list_of_integers[cen] < list_of_integers[cen - 1]:
-        return find_peak(list_of_integers[:cen])
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
