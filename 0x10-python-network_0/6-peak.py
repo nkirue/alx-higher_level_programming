791fd5a62243fed729e8636a35e113410b1c4df4
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-01-11 08:52:17 -0800

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""
-Module for finding a peak in a list of unsorted integers.
-"""
+"""Finds a peak in a list of unsorted integers"""
+
 
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
+    """Finds a peak in list_of_integers"""
 
-    Args:
-    - list_of_integers (list): List of unsorted integers
-
-    Returns:
-    - int: A peak element from the list
-    """
-
-    # Check if the list is empty
-    if not list_of_integers:
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    # Calculate the cendle index
-    cen = len(list_of_integers) // 2
-
-    # Compare the cendle element with its neighbors
-    if (cen == 0 or list_of_integers[cen] >= list_of_integers[cen - 1]) and \
-       (cen == len(list_of_integers) - 1 or list_of_integers[cen] >= list_of_integers[cen + 1]):
+   lo_ = 0
+    hi = len(list_of_integers)
+    cen = ((hi -lo_) // 2) +lo_
+    cen = int(cen)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[cen] >= list_of_integers[cen - 1] and\
+            list_of_integers[cen] >= list_of_integers[cen + 1]:
         return list_of_integers[cen]
-
-    # If the cendle element is not a peak, recurse on the side with the larger neighbor
-    if cen > 0 and list_of_integers[cen - 1] > list_of_integers[cen]:
+    if cen > 0 and list_of_integers[cen] < list_of_integers[cen + 1]:
+        return find_peak(list_of_integers[cen:])
+    if cen > 0 and list_of_integers[cen] < list_of_integers[cen - 1]:
         return find_peak(list_of_integers[:cen])
-    else:
-        return find_peak(list_of_integers[cen + 1:])
